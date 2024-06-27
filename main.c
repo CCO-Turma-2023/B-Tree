0fcd3d8f7accb798d1d70cdb56b6a8bffbae7264
CaioMD <caiomd6@gmail.com>
2024-06-27 20:29:16 -0300

    Versão mais atualizada da split.

--- main.c
+++ main.c
@@ -1,15 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "btree.h"
 
 #define MAX_NOME 8
 
 struct aluno{
     int matricula;
-    char nome[100];
+    char nome[51];
 };
+
+int* gerarEntrada (int n){
+    int *v = (int*)malloc(sizeof(int) * n), indiceV, indiceU, aux;
+    if (!v)
+    {
+        return NULL;
+    }
+
+    FILE *arq = fopen("entrada2.txt", "w+");
+    FILE *nomes = fopen("nomes.txt", "r");
+    char s[51];
+    if (!arq)
+    {
+        return NULL;
+    }
+
+
+    for (int i = 0; i < n; i++)
+    {
+        v[i] = 2023000000 + i;
+    }
+
+    for (int i = 0; i < 1494912; i++)
+    {
+        indiceV = rand() % n;
+        indiceU = rand() % n;
+        aux = v[indiceV];
+        v[indiceV] = v[indiceU];
+        v[indiceU] = aux;
+    }
+
+    for (int i = 0; i < n; i++)
+    {   fscanf (nomes, "%[^\n]\n", s);
+        fprintf (arq, "%d %s\n", v[i], s);
+    }
+    fclose(arq);
+    return v;
+}
 
 int main() {
     int ordem;
+    int *v = gerarEntrada(10000);
+    if (!v)
+        return 1;
     printf ("Digite a ordem da arvore(Obs. Somente numeros pares e maior ou igual a 4):");
     scanf ("%d", &ordem);
     if (ordem%2 || ordem < 3){
@@ -18,8 +60,8 @@ int main() {
     }
     Btree *arvore = criarArvore(ordem);
     chave aux;
-    int vetor[200];
-    FILE *arq = fopen("entrada.txt", "r");
+    int vetor[10000];
+    FILE *arq = fopen("entrada2.txt", "r");
     struct aluno aluno;
     int indice = 1, indiceArq;
     while (!feof(arq)) {
@@ -30,17 +72,22 @@ int main() {
         insereChave(arvore, aux);
         indice++;
     }
-    for (int j = 0; j < indice-1; j++) {
+    int oi = 0;
+    for (int j = 0; j < indice-1; j+=50) {
         indiceArq = busca(getRaiz(arvore), vetor[j]);
         if (indiceArq == 0) {
             printf("Elemento nao encontrado\n");
         } else {
+            oi++;
+            printf ("%d - ", indiceArq);
             fseek(arq, 0, SEEK_SET);
-            for (int i = 0; i < indiceArq - 1; i++)
-                fscanf(arq, "%d  %[^\n]", &aluno.matricula, aluno.nome);
+            fseek(arq, indiceArq * (sizeof(int) + sizeof(char) * 51), SEEK_SET);
+            //for (int i = 0; i < indiceArq - 1; i++)
+                //fscanf(arq, "%d  %[^\n]", &aluno.matricula, aluno.nome);
             fscanf(arq, "%d  %[^\n]", &aluno.matricula, aluno.nome);
             printf("%d - %s\n", aluno.matricula, aluno.nome);
         }
     }
+    printf ("\n%d", oi);
     return 0;
 }
