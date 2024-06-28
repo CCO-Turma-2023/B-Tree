621b5abd12b5d5b23256d19177f2bed019c9c331
Gabriel Alves <gabhenriquesa1@gmail.com>
2024-06-27 22:14:19 -0300

    Adicionado comentários e melhorias na identação

--- btree.c
+++ btree.c
@@ -59,19 +59,20 @@ Btree* criarArvore(int ordem) {
 
 void addChave(no *aux, chave novaChave) {
     int indice = aux->n;
-    // Procurando da última posição ocupada para achar o local para inserir a chave
+    // Procurando o local certo para inserir a chave
     while (indice != 0 && novaChave.valor < aux->chaves[indice - 1].valor) {
-        aux->chaves[indice] = aux->chaves[indice-1];
-        aux->filhos[indice+1] = aux->filhos[indice];
+        aux->chaves[indice] = aux->chaves[indice-1]; // Dá um shift para a direita na chave
+        aux->filhos[indice+1] = aux->filhos[indice]; // Dá um shift para a direita no filho
         indice--;
     }
-    aux->n++;
-    aux->chaves[indice] = novaChave;
+    aux->n++; // Aumenta o número de chaves
+    aux->chaves[indice] = novaChave; // Insere a chave
 }
 
 no* split(Btree *arv, no *aux, chave chaveNova){
     // Alocando um novo nó para dividir o nó aux
-    no *novoNo = alocaNo(arv->ordem), *pai;
+    no *novoNo = alocaNo(arv->ordem); // Aloca espaço para um novo nó
+    no *pai; // Prepara variável do pai
     int indice, i, tamanho = aux->n;
     chave meio;
 
@@ -79,65 +80,67 @@ no* split(Btree *arv, no *aux, chave chaveNova){
     if (!novoNo){
         return NULL;
     }
-    novoNo->folha = aux->folha;
+
+    novoNo->folha = aux->folha; // Define a mesma propriedade de folha no nó irmão
     indice = 0;
-    for (i = (aux->n/2) +1; i < tamanho; i++){
-        novoNo->chaves[indice] = aux->chaves[i];
-        novoNo->filhos[indice] = aux->filhos[i];
-        novoNo->filhos[indice + 1] = aux->filhos[i + 1];
-        if (novoNo->filhos[indice]) {
-            novoNo->filhos[indice]->pai = novoNo;
-            novoNo->filhos[indice + 1]->pai = novoNo;
+    for (i = (aux->n/2) +1; i < tamanho; i++){ // Copia a segunda metade do nó aux para o novo nó
+        novoNo->chaves[indice] = aux->chaves[i]; // Copia a chave para o novo nó
+        novoNo->filhos[indice] = aux->filhos[i]; // Copia o filho à esquerda da chave antiga pro novo nó
+        novoNo->filhos[indice + 1] = aux->filhos[i + 1]; // Copia o filho à direita da chave antiga pro novo nó
+        if (novoNo->filhos[indice]) { // Caso os filhos da chave que foi transferida exista
+            novoNo->filhos[indice]->pai = novoNo; // Seta o pai do filho para o novo nó criado
+            novoNo->filhos[indice + 1]->pai = novoNo; // Seta o pai do filho para o novo nó criado
         }
-        novoNo->n++;
-        aux->n--;
-        indice ++;
+        novoNo->n++; // Aumenta o número de chaves no novo nó
+        aux->n--; // Diminui o número de chaves no nó antigo
+        indice ++; // Incrementa o índice
     }
+
     meio = aux->chaves[aux->n - 1];
-    if (aux == arv->raiz){
-        no *novaRaiz = alocaNo(arv->ordem);
+    if (aux == arv->raiz){ // Se o nó que foi dividido é a raiz
+        no *novaRaiz = alocaNo(arv->ordem); // Aloca uma nova raiz
         if (!novaRaiz){
             return NULL;
         }
-        aux->n--;
-        novaRaiz->chaves[0] = meio;
-        novaRaiz->n = 1;
-        novaRaiz->filhos[0] = aux;
-        novaRaiz->filhos[1] = novoNo;
-        novaRaiz->folha = 0;
-        aux->pai = novoNo->pai = novaRaiz;
-        arv->raiz = novaRaiz;
+        aux->n--; // Diminui o número de chaves no nó antigo
+        novaRaiz->chaves[0] = meio; // A raiz recebe a chave do meio
+        novaRaiz->n = 1; // Aumenta o número de chaves na nova raiz
+        novaRaiz->filhos[0] = aux; // O filho da esquerda da nova raiz é o nó antigo
+        novaRaiz->filhos[1] = novoNo; // O filho da direita da nova raiz é o novo nó
+        novaRaiz->folha = 0; // A nova raiz não é folha
+        aux->pai = novoNo->pai = novaRaiz; // Seta o pai dos nós
+        arv->raiz = novaRaiz; // A nova raiz da árvore é a nova raiz criada
     } else {
-        if (aux->pai->n == arv->ordem - 1){
-            pai = split(arv, aux->pai, meio);
-            if (meio.valor < pai->chaves[0].valor) {
-                pai = aux->pai;
+        if (aux->pai->n == arv->ordem - 1){ // Se o pai do nó estiver cheio !!!!!!!!!!!!!
+            pai = split(arv, aux->pai, meio); // Split  no pai
+            if (meio.valor < pai->chaves[0].valor) { // Se a chave do meio for menor que a primeira chave do pai
+                pai = aux->pai; // O pai passa a ser o pai do nó antigo
             }
-            aux->n--;
-            indice = pai->n;
-            while (indice != 0 && meio.valor != pai->chaves[indice].valor) {
-                indice--;
+            aux->n--; // Diminui o número de chaves no nó antigo
+            indice = pai->n; // Índice recebe o número de chaves do pai
+            while (indice != 0 && meio.valor != pai->chaves[indice].valor) { // Encontra a posição correta para inserir a chave
+                indice--; // Decrementa o índice
             }
-            novoNo->pai = pai;
+            novoNo->pai = pai; // O pai do novo nó é o pai do nó antigo
             pai->filhos[indice + 1] = novoNo;
-        } else {
+        } else { // Se o pai do nó não estiver cheio
             pai = aux->pai;
             indice = pai->n;
-            while (indice != 0 && chaveNova.valor < pai->chaves[indice - 1].valor) {
-                pai->chaves[indice] = pai->chaves[indice - 1];
-                pai->filhos[indice + 1] = pai->filhos[indice];
-                indice--;
+            while (indice != 0 && chaveNova.valor < pai->chaves[indice - 1].valor) { // Encontra a posição correta para inserir a chave
+                pai->chaves[indice] = pai->chaves[indice - 1]; // Dá um shift para a direita na chave
+                pai->filhos[indice + 1] = pai->filhos[indice]; // Dá um shift para a direita no filho
+                indice--; // Decrementa o índice
             }
-            pai->chaves[indice] = meio;
-            novoNo->pai = aux->pai;
-            pai->filhos[indice + 1] = novoNo;
-            pai->n++;
-            aux->n--;
+            pai->chaves[indice] = meio; // Insere o elemento do meio do aux no pai
+            novoNo->pai = aux->pai; // O pai do novo nó é o pai do nó antigo
+            pai->filhos[indice + 1] = novoNo; // O filho à direita da chave antiga é o novo nó
+            pai->n++; // Aumenta o número de chaves no pai
+            aux->n--; // Diminui o número de chaves no nó antigo
         }
     }
 
-    if (meio.valor < chaveNova.valor){
-        aux = novoNo;
+    if (meio.valor < chaveNova.valor){ // Se o valor inserido for maior que o meio do nó antigo
+        aux = novoNo;  // Seta o aux para o novo nó
     }
 
     addChave(aux, chaveNova);
@@ -151,59 +154,64 @@ int insereChave(Btree *arv, chave novaChave) {
     int indice;
 
     // Achando o nó folha onde será inserido o valor
-    while (!aux->folha){
+    while (!aux->folha) { // Enquanto não for folha
         indice = 0;
-        while (novaChave.valor > aux->chaves[indice].valor && indice < aux->n) {
+        while (novaChave.valor > aux->chaves[indice].valor && indice < aux->n) { // Encontra nó folha para inserir
             indice++;
         }
         aux = aux->filhos[indice];
     }
 
-    // Achando onde a chave será inserida no nó
-    if (aux->n != arv->ordem - 1) {
+    // Verifica se o nó está cheio
+    if (aux->n != arv->ordem - 1) { // Se não estiver cheio, chama a função addChave
         addChave(aux, novaChave);
-    } else {
+    } else { // Se estiver cheio, chama a função split
         split(arv, aux, novaChave);
     }
     return 1;
 }
 
-no* getRaiz (Btree *arv){
+no* getRaiz (Btree *arv) {
     return arv->raiz;
 }
 
-void imprimirArvore(no *aux){
-    if (aux){
+void imprimirArvore(no *aux) {
+    if (aux) {
         for (int i = 0; i < aux->n; i++) {
             printf("%d ", aux->chaves[i].valor);
         }
         printf ("- pai: ");
         if (aux->pai)
-        for (int i = 0; i < aux->pai->n; i++) {
-            printf("%d ", aux->pai->chaves[i].valor);
-        }
-        printf ("\n");
+            for (int i = 0; i < aux->pai->n; i++) {
+                printf("%d ", aux->pai->chaves[i].valor);
+            }
+
+        printf("\n");
         if (aux->filhos[0])
             printf ("filhos: ");
-            for (int i = 0; i < aux->n + 1; i++){
-                imprimirArvore(aux->filhos[i]);
+        for (int i = 0; i < aux->n + 1; i++){
+            imprimirArvore(aux->filhos[i]);
         }
     }
     return;
 }
 
-int busca (no *aux, int nroMatricula){
+int busca(no *aux, int nroMatricula) {
     int i = 0;
     // Encontre a primeira chave maior ou igual a k
-    while (i < aux->n && nroMatricula > aux->chaves[i].valor)
+    while (i < aux->n && nroMatricula > aux->chaves[i].valor) {
         i++;
+    }
+
     // Se a chave for encontrada, retorne o nó
-    if (aux->chaves[i].valor == nroMatricula)
+    if (aux->chaves[i].valor == nroMatricula) {
         return aux->chaves[i].indice;
+    }
 
     // Se a chave não for encontrada aqui e este é um nó folha
-    if (aux->folha == 1)
+    if (aux->folha == 1) {
         return 0;
+    }
 
     // Vá para o filho apropriado
     return busca(aux->filhos[i], nroMatricula);
